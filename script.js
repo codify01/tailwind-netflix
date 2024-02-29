8b12fc98fa1a5e41c5b029ac53a823faa363ee7f
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-02-29 15:03:13 +0100

    init commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let acc = document.getElementsByClassName("item");
+let index
+
+for (let index = 0; index < acc.length; index++) {
+    acc[index].addEventListener('click', (e)=> {
+       e.currentTarget.closest(".item").classList.toggle('active');
+    })
+}
+
+
 
     tailwind.config = {
       theme: {
